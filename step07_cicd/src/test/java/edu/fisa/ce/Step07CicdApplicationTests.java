81bd8354418f8a670e75a16a567d9e496359a5e2
letmeloveyou82 <shineanl04@gmail.com>
2025-04-09 09:45:41 +0900

    task3. update test src

--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -1,5 +1,7 @@
 package edu.fisa.ce;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 class Step07CicdApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testAddition() {
+		int result = 2 + 3;
+		/* 예시 - 서버 시작 시에 특정 Database로부터 검색
+		 * 그 데이터는 꼭 fisa여야만 하는 상황
+		 * fisa가 없다면 서버 중지 또는 다른 로직 실행
+		 * 
+		 * 개발 중에 데이터에 대한 test 검증용등으로 사용
+		 */
+		System.out.println("비교 전*************");
+		assertEquals(5, result);
+		System.out.println("비교 후*************");
 	}
-
 }
